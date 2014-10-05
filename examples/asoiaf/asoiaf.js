a0e9c690cc35e1d8e46727beb82835c86062e783
Brent Hagany <brent@hiidef.com>
2014-10-05 00:36:28 -0500

    Remove js version

--- examples/asoiaf/asoiaf.js
+++
@@ -1,6 +0,0 @@
-(function(window) {
-    var debug = null; //{'features': ['nodes'], 'wireframe': false};
-    var conf = {'group_styles': {'pov': {'stroke-width': 3}},
-                'debug': debug};
-    var chart = new NChart('paper', asoiaf.characters, asoiaf.layers, conf).calc().plot().draw();
-})(window);
